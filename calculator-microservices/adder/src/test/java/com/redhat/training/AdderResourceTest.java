c831d2c675e37e86952e864305729bb75dcf331c
Haythem Trabelsi <haythem.trabeelsi@gmail.com>
2022-12-22 11:19:04 +0100

    refactor

--- calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
+++ calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
@@ -7,7 +7,13 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 @QuarkusTest
 @TestHTTPEndpoint(AdderResource.class)
 public class AdderResourceTest {
@@ -15,5 +21,36 @@ public class AdderResourceTest {
     @InjectMock
     @RestClient 
     SolverService solverService;
-
+@Test
+public void simpleSum() {
+Mockito.when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+given()
+.when().get("3/2")
+.then()
+.statusCode(200)
+.body(is("5.0"));
+}
+@Test
+public void negativeSum() {
+Mockito.when(solverService.solve("-2")).thenReturn(Float.valueOf("-2"));
+Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+given()
+.when().get("3/-2")
+.then()
+.statusCode(200)
+.body(is("1.0"));
+}
+@Test
+public void wrongValue() {
+WebApplicationException cause = new WebApplicationException("Unknown error",
+Response.Status.BAD_REQUEST);
+Mockito.when(solverService.solve("a")).thenThrow( new
+ResteasyWebApplicationException(cause) );
+Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+given()
+.when().get("3/a")
+.then()
+.statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+}
 }
--- calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
+++ calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
@@ -2,9 +2,19 @@ package com.redhat.training;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.jupiter.api.Test;
+import javax.ws.rs.core.Response;
 @QuarkusTest
 @TestHTTPEndpoint(SolverResource.class)
 public class SolverResourceTest {
-
+@Test
+public void simpleSum() {
+given()
+.when().get("3+2")
+.then()
+.statusCode(200)
+.body(is("5.0"));
+}
 }
